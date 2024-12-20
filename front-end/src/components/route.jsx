58b3190e370521fad9ecf06d98af9d5f0f83b0d7
anna.dosev <anna.dosev@world50.com>
2024-12-20 08:43:35 -0800

    add some file updates

--- front-end/src/components/route.jsx
+++ front-end/src/components/route.jsx
@@ -1,47 +1,53 @@
-import '../styles/routeStyles.css';
-import route from '../assets/photos/route.png'
-import dot1 from '../assets/photos/dots/dot1.png'
-import dot2 from '../assets/photos/dots/dot2.png'
-import dot3 from '../assets/photos/dots/dot3.png'
-
+import "../styles/routeStyles.css";
+import route from "../assets/photos/route.png";
+import dot1 from "../assets/photos/dots/dot1.png";
+import dot2 from "../assets/photos/dots/dot2.png";
+import dot3 from "../assets/photos/dots/dot3.png";
 
 const Route = () => {
-    return (
-        <div className="route-container">
-          <h1 className="route-header">
-              Route and Timeline
-          </h1>
-          <div className="route-info-boxes">
-            <div className="info-box">11,500 miles</div>
-            <div className="info-box">174 days</div>
-            <div className="info-box">19 states</div>
-          </div>
-          <div className="cities-list">
+  return (
+    <div className="route-container">
+      {/* <h1 className="route-header">Route and Timeline</h1> */}
+      {/* <div className="route-info-boxes">
+        <div className="info-box">11,500 miles</div>
+        <div className="info-box">174 days</div>
+        <div className="info-box">19 states</div>
+      </div> */}
+      {/* <div className="cities-list">
               Jacksonville • Pensacola • Memphis • Wichita • Denver • Salt Lake City • Boise • Hood River • Cannon Beach • Seattle • Portland • Crescent City • Napa Valley • San Fransisco • 
               Santa Cruz • Big Sur • Santa Barbara • Los Angeles • San Diego • Mammoth Lakes • Tahoe • Salt Lake City • Jackson Hole • Zion • Las Vegas • Flagstaff • Sedona • 
               Durango • Santa Fe • Amarillo • Dallas 
+          </div> */}
+      <div className="map-container">
+        <div className="map-image">
+          <img src={route} alt="" />
+        </div>
+        <div className="map-elements">
+          <div className="map-keys">
+            <img src={dot2} alt="" />
+            <p className="map-text">
+              Initial route from Jax Beach to Cannon Beach <br /> Ten days from
+              coast to coast{" "}
+            </p>
           </div>
-          <div className="map-container">
-            <div className="map-image">
-              <img src={route} alt=""/>
-            </div>
-            <div className="map-elements">
-                <div className="map-keys">
-                  <img src={dot2} alt=""/>
-                  <p className="map-text">Initial route from Jax Beach to Cannon Beach <br/> Ten days from coast to coast </p>
-                </div>
-                <div className="map-keys">
-                  <img src={dot1} alt=""/>
-                  <p className="map-text">Main backbone of the route <br/> The time spent in one place varied from one night to four weeks</p>
-                </div>
-                <div className="map-keys">
-                  <img src={dot3} alt=""/>
-                  <p className="map-text">Weekend trips and noteable pit-stops<br/> Mt. Rainer • Lake Tahoe • Tetons • Zion</p>
-                </div>
-            </div>
+          <div className="map-keys">
+            <img src={dot1} alt="" />
+            <p className="map-text">
+              Main backbone of the route <br /> The time spent in one place
+              varied from one night to four weeks
+            </p>
+          </div>
+          <div className="map-keys">
+            <img src={dot3} alt="" />
+            <p className="map-text">
+              Weekend trips and noteable pit-stops
+              <br /> Mt. Rainer • Lake Tahoe • Tetons • Zion
+            </p>
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
 export default Route;
--- front-end/src/skeleton/views/about.jsx
+++ front-end/src/skeleton/views/about.jsx
@@ -2,15 +2,61 @@ import Route from "../../components/route";
 // import React from "react";
 import Gallery from "../../components/gallery";
 import heroPic from "../../assets/photos/hero.jpg";
+import heroPic2 from "../../assets/photos/moab1.JPG";
+import heroPic3 from "../../assets/photos/moab2.jpg";
+import logo from "../../assets/photos/logo2.png";
+import pnw5 from "../../assets/photos/pnw/pnw5.png";
 import "../../styles/routeStyles.css";
+import "../../styles/aboutStyles/aboutStyles.css";
 
 const About = () => {
   return (
     <div>
-      <div className="blog-welcome-container-image">
-        <img className="blog-heroPic" src={heroPic} alt="" />
-        <div className="blog-siteName-container">
-          <h1 className="blog-siteName">My Way Around the USA</h1>
+      <div className="about-hero-container-image">
+        <img className="about-heroPic" src={heroPic2} alt="" />
+        <div className="blog-hero-text-container">
+          <h1 className="blog-siteName">My Story</h1>
+          <p className="blog-subtitle">Subtitle TBD</p>
+        </div>
+      </div>
+      <div className="main-content-container">
+        <div className="text-blurb-content-container">
+          <p className="blurb-about">
+            Eventually I will write something here. Was mainly focused on
+            layout.
+            <br></br>
+            <br></br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+            <br></br>
+          </p>
+          <div className="explore-text-container">
+            <h1 className="explore-title">How it all Began</h1>
+          </div>
+          <p className="remaining-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+
+          <p className="remaining-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
         </div>
       </div>
       <Route />
